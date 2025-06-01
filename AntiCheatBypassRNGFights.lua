67d354f8e84c722b05b76c41bc7b3793c12947d8
Xeric Hub <xerichub@gmail.com>
2025-06-01 15:15:29 +0300

    Update AntiCheatBypassRNGFights.lua

--- AntiCheatBypassRNGFights.lua
+++ AntiCheatBypassRNGFights.lua
@@ -1,3 +1,15 @@
+local StarterGui = game:GetService("StarterGui")
+
+StarterGui:SetCore("SendNotification", {
+    Title = "Hello!",
+    Text = "This script is to bypass the anti-cheat of RNG Fights, the anti-cheat is being removed.",
+    Duration = 3600,
+    Button1 = "OK",
+    Callback = function()
+        print("Notification closed")
+    end
+})
+
 print("|| Xeric Hub || working on it...")
 a=function(b)b=string.lower(b)return string.find(b,"anti")or string.find(b,"cheat")or string.find(b,"meter")end
 c=function(d)for _,e in ipairs(d:GetDescendants())do if e:IsA("Instance")and e.Name and a(e.Name)then pcall(function()e:Destroy()end)end end end
